85c05e785d2f6e309f5b7481937f2df51f63fda4
Aluno <evelyn.santos@bandtec.com.br>
2019-12-04 17:27:28 -0200

    site pessoal

--- site/models/usuario.js
+++ site/models/usuario.js
@@ -5,69 +5,59 @@ lista e explicação dos Datatypes:
 https://codewithhugo.com/sequelize-data-types-a-practical-guide/
 */
 
-module.exports = (sequelize, DataTypes) => {  
-    let Usuario = sequelize.define('Usuario',{
-		
+module.exports = (sequelize, DataTypes) => {
+	let Usuario = sequelize.define('Usuario', {
+
 		idcadastro: {
 			field: 'idcadastro',
 			type: DataTypes.INTEGER,
 			primaryKey: true,
 			autoIncrement: false
-		},		
-		
+		},
+
 		nome: {
 			field: 'nome',
 			type: DataTypes.STRING,
 			allowNull: false
 		},
 
-		endereço: {
-			field: 'endereço',
+		endereco: {
+			field: 'endereco',
 			type: DataTypes.STRING,
 			allowNull: false
 		},
-		
+
 		numero: {
 			field: 'numero',
-			type: DataTypes.STRING,
+			type: DataTypes.INTEGER,
 			allowNull: false
 		},
 		contato: {
 			field: 'contato',
 			type: DataTypes.STRING,
 			allowNull: false
-		
-	  },
-	    quantidadeflores: {
-		field: 'quantidadeflores',
-		type: DataTypes.STRING,
-		allowNull: false
-
-	}, 
-     cores: {
-     field: 'cores',
-     type: DataTypes.STRING,
-     allowNull: false
-	 },
-	 email: {
-		field: 'email',
-		type: DataTypes.STRING,
-		allowNull: false
+		},
+		email: {
+			field: 'email',
+			type: DataTypes.STRING,
+			allowNull: false
 		},
 		senha: {
 			field: 'senha',
 			type: DataTypes.STRING,
 			allowNull: false
-			},
-	
+		}
+		
+		},
+		{
 
-               
-		tableName: 'Usuario', 
-		freezeTableName: true, 
+
+		tableName: 'cadastrouser',
+		freezeTableName: true,
 		underscored: true,
 		timestamps: false,
 
 	});
 
-    return Usuario;
+	return Usuario;
 };
--- site/routes/usuarios.js
+++ site/routes/usuarios.js
@@ -9,10 +9,10 @@ let sessoes = [];
 router.post('/autenticar', function(req, res, next) {
 	console.log('Recuperando usuário por login e senha');
 
-	var login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var login = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
 	
-	let instrucaoSql = `select * from usuario where login='${login}' and senha='${senha}'`;
+	let instrucaoSql = `select * from cadastrouser where login='${login}' and senha='${senha}'`;
 	console.log(instrucaoSql);
 
 	sequelize.query(instrucaoSql, {
@@ -37,13 +37,16 @@ router.post('/autenticar', function(req, res, next) {
 });
 
 /* Cadastrar usuário */
-router.post('/', function(req, res, next) {
+router.post('/cadastrar', function(req, res, next) {
 	console.log('Criando um usuário');
 	
 	Usuario.create({
 		nome : req.body.nome,
-		login : req.body.login,
-		senha: req.body.senha
+		endereco : req.body.endereco,
+		numero: req.body.numero,
+		contato: req.body.contato,
+		email:req.body.email,
+		senha:req.body.senha
 	}).then(resultado => {
 		console.log(`Registro criado: ${resultado}`)
         res.send(resultado);
