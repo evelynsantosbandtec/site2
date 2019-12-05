3a7c43f8a15134195f2173cfbb7edec868877b56
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-12-04 23:34:12 -0200

    site pessoal

--- documentacao/script-atual.sql
+++ documentacao/script-atual.sql
@@ -3,12 +3,13 @@ use cadastro;
 create table cadastrouser 
 (idcadastro int primary key auto_increment ,
 nome varchar (40),
-endereço varchar(50),
+endereco varchar(50),
 numero int ,
 contato char(11),
-quantidadedeflores varchar(40),
-cor varchar(40),
 email varchar (40),
 senha varchar (30)
 );
 select * from cadastrouser ;
+
+
+   
